4659cc168c400f5a303a331a402845f4c7db3467
leaf <leaf@leaf.sx>
2017-09-24 16:20:14 +0100

    Update & fix open port check stuff (NetworkHelper)

--- SM64O/NetworkHelper.cs
+++ SM64O/NetworkHelper.cs
@@ -13,12 +13,14 @@ namespace SM64O
         public static string ExternalIp;
         public static int ConfirmedOpenPort;
 
-        private const string ServiceAddress = "api.sm64o.com";
+        private const string ServiceAddress = "napi.sm64o.com";
         private const int ServicePort = 6460;
 
         private static EndPoint Service = null;
 
         private static bool _success; // this is dirty
+
+        private static bool _errOnce = false; // only show err message once
 
         public static async Task<bool> RequestAssistance(int port)
         {
@@ -70,41 +72,56 @@ namespace SM64O
             if (e.Sender.GetHashCode() != Service.GetHashCode())
                 return;
 
-            // read our IP
-            IPAddress add = new IPAddress(e.Data);
-            ExternalIp = add.ToString();
-            /*
-            
-            // Don't need to send back ACK
+            // check response data
+            var response = System.Text.Encoding.ASCII.GetString(e.Data);
 
-            byte[] ack = new byte[4];
+            if (response.StartsWith("BADVER"))
+            {
+                if (!_errOnce)
+                    System.Windows.Forms.MessageBox.Show("Failed to communicate with Net64 open port checker - your application is out-of-date.", "Out Of Date", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
+                _errOnce = true;
+                return;
+            }
 
-            ack[0] = 0x41;
-            ack[0] = 0x43;
-            ack[0] = 0x4b;
-            ack[0] = 0x00;
+            if (response.StartsWith("OK "))
+            {
+                // read our IP
+                var wbf = response.Substring(3);
+                IPAddress add = IPAddress.Parse(wbf);
+                ExternalIp = add.ToString();
+                /*
 
-            ((UdpConnectionListener)Form1.listener).SendUnconnectedBytes(ack, Service);
-            */
-            _success = true;
+                // Don't need to send back ACK
+
+                byte[] ack = new byte[4];
+
+                ack[0] = 0x41;
+                ack[0] = 0x43;
+                ack[0] = 0x4b;
+                ack[0] = 0x00;
+
+                ((UdpConnectionListener)Form1.listener).SendUnconnectedBytes(ack, Service);
+                */
+                _success = true;
+            }
         }
 
         private static byte[] BuildStartPacket(short port)
         {
-            byte[] buffer = new byte[64];
+            byte[] buffer = new byte[400];
 
-            buffer[0] = 0x73;
-            buffer[1] = 0x6d;
-            buffer[2] = 0x36;
-            buffer[3] = 0x34;
-            buffer[4] = 0x6f;
-            buffer[5] = 0x69;
-            buffer[6] = 0x70;
-            buffer[7] = 0x63;
-            buffer[8] = 0x30;
-            buffer[9] = 0x30;
-            buffer[10] = 0x30;
-            buffer[11] = 0x30;
+            buffer[0] = 0x6e; // 'n'
+            buffer[1] = 0x65; // 'e'
+            buffer[2] = 0x74; // 't'
+            buffer[3] = 0x36; // '6'
+            buffer[4] = 0x34; // '4'
+            buffer[5] = 0x69; // 'i'
+            buffer[6] = 0x70; // 'p'
+            buffer[7] = 0x63; // 'c'
+            buffer[8] = 0x30; // '0'
+            buffer[9] = 0x30; // '0'
+            buffer[10] = 0x30; // '0'
+            buffer[11] = 0x30; // '0'
 
             Array.Copy(BitConverter.GetBytes(port), 0, buffer, 12, 2);
 
